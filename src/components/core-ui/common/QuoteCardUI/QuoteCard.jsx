18872946b03a6904abfd73ece9abaef1f8e9724f
DanBec <65568682+dapabe@users.noreply.github.com>
2022-04-03 17:09:23 -0300

    update

--- src/components/core-ui/common/QuoteCardUI/QuoteCard.jsx
+++ src/components/core-ui/common/QuoteCardUI/QuoteCard.jsx
@@ -1,28 +1,33 @@
 import { ExternalLinkIcon } from "@heroicons/react/solid";
 import React from "react";
 
-export default function QuoteCard({
+const QuoteCard =React.forwardRef({ref,
   cite,
   image,
   image_desc,
   nickname,
   quote,
-}) {
-  return (
+}) =>  (
     <blockquote
-      className="relative flex min-h-[132px] max-w-md rounded-full bg-white py-3 text-sutilBlack sm:rounded-l-none sm:pr-16 mxsm:rounded-md mxsm:px-14"
+      className="relative flex min-h-[132px] max-w-md rounded-full bg-white py-3 text-sutilBlack sm:rounded-l-none sm:pr-14 mxsm:rounded-md mxsm:pl-10 mxsm:pr-14"
       cite={cite}
     >
       <img
-        src={`/images/inspirations/${image}`}
+      ref={ref}
+        src={`/images/inspirations/${image}` || "/images/placeholder.jpg"}
         alt={image_desc}
         className="absolute top-0 h-full min-w-[132px] -translate-x-1/2 overflow-hidden rounded-full object-cover mxsm:hidden "
       />
-      <div className="sm:ml-20">
-        <p className="text-sm">
+      <div className="sm:ml-24">
+        <p>
           <q>{quote}</q>
-          <a href={cite} target="_blank" rel="noopener noreferrer">
-            <ExternalLinkIcon className="inline-block w-4" />
+          <a
+            href={cite}
+            target="_blank"
+            rel="noopener noreferrer"
+            title="Ir al origen de esta cita."
+          >
+            <ExternalLinkIcon className="inline-block w-5" />
           </a>
         </p>
         <h1 className="mt-3 text-right font-semibold">{nickname}</h1>
@@ -30,8 +35,9 @@ export default function QuoteCard({
       <img
         src="/images/quote.png"
         alt="Simbolo de cita textual"
-        className="absolute right-0 top-0 h-14 w-14"
+        className="absolute -right-2 top-0 h-14 w-14"
       />
     </blockquote>
   );
-}
+
+export default QuoteCard
