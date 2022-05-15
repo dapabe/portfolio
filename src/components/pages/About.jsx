e645c41b3b12c4b9f0bbd5ce676d3ea7fca54e1e
DanBec <65568682+dapabe@users.noreply.github.com>
2022-05-15 15:35:53 -0300

    about page redone

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -5,37 +5,46 @@ export default function About() {
     <section className="about noMaxWidth ">
       <section className="mx-auto mt-10 max-w-xl space-y-6">
         <h1 className="paragraphTitle text-center underline underline-offset-4">
-          como empece
+          COMO EMPECE
         </h1>
         <p>
-          Desde pequeño me interesé por las computadoras gracias a eso he podido
-          aprender inglés desde muy temprana edad y adentrarme aún más en este
-          mundo.
+          Mi interés por la programación proviene desde el primer contacto que
+          tuve con una computadora, me pregunté como es que funcionaban las
+          herramientas y programas en ella. <br />
+          Gracias a mis estudios y el aprender el idioma Inglés pude comprender
+          como eran posibles estas máquinas, ya que mucha de esta información
+          existe en ese lenguaje.
           <br />
-          <br />A mis 20 años tuve un "clic" por la programación, creé{" "}
-          <AbbrLang title="Modificaciones">mods</AbbrLang> para juegos, por
-          ejemplo{" "}
-          <ExtLink href="https://steamcommunity.com/sharedfiles/filedetails/?id=2380720925">
-            Starbound
+          <br />A mis 20 años tuve interés en crear modificaciones para juegos y
+          comprendí como funcionaba la{" "}
+          <ExtLink
+            href="https://es.wikipedia.org/wiki/Programaci%C3%B3n_orientada_a_objetos"
+            title="Ir a la definición de que es OOP"
+          >
+            <abbr title="Object-oriented programming">OOP</abbr>
           </ExtLink>
-          , y luego me enfoqué en el desarrollo web.
+          , programación orientada a objetos.
         </p>
-
         <h2 className="paragraphTitle text-center underline underline-offset-4">
-          al día de hoy
+          AL DÍA DE HOY
         </h2>
         <p>
-          Cuando comencé con las herramientas básicas{" "}
+          Deje a un lado mi{" "}
+          <AbbrLang title="Afición u ocupación">hobby</AbbrLang> de crear{" "}
+          <AbbrLang title="Modificaciones">mods</AbbrLang> para juegos y me
+          especialice en el desarrollo web. <br />
+          Al dominar estos 3 lenguajes;{" "}
           <abbr title="Hyper Text Markup Language">HTML</abbr>,{" "}
-          <abbr title="Cascade Style Sheets">CSS</abbr> y JavaScript todo
-          parecía ir muy lento, pero seguro, hasta que conocí{" "}
-          <AbbrLang title="Marcos de trabajo">frameworks</AbbrLang> como{" "}
+          <abbr title="Cascade Style Sheets">CSS</abbr> y JavaScript, decidí
+          investigar que herramientas de desarrollo web o{" "}
+          <AbbrLang title="Marcos de trabajo">frameworks</AbbrLang> eran los más
+          usadas, así es como me enfoque en{" "}
           <ExtLink href="https://es.reactjs.org/">React.JS</ExtLink> y{" "}
-          <ExtLink href="https://nextjs.org/">NEXT.JS</ExtLink>
-          , estos me despertaron la pasión por el desarrollo web.
+          <ExtLink href="https://nextjs.org/">NEXT.JS</ExtLink>.
           <br />
-          <br />Y desde ello cada día he querido ir aprendiendo y ampliar más mi
-          saber por las nuevas tecnologías que existen, y que existirán.
+          <br />
+          Ahora estoy en camino de dominar estos y aprender más lenguajes, y
+          frameworks ampliando más mi conocimiento.
         </p>
       </section>
     </section>
