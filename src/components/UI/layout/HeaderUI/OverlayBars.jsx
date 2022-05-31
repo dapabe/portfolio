f9347f66e67f6433359e8f10b0ae9c3ff437c69d
DanBec <65568682+dapabe@users.noreply.github.com>
2022-05-31 17:07:19 -0300

    fixed breakpoints from CSS

--- src/components/UI/layout/HeaderUI/OverlayBars.jsx
+++ src/components/UI/layout/HeaderUI/OverlayBars.jsx
@@ -1,8 +1,8 @@
 const bars = [
   "h-10 w-full",
-  "w-10 right-0 h-[calc(100%-2.5rem)] translate-x-full sm:translate-x-0",
-  "h-10 right-0 bottom-0 w-[calc(100%-2.5rem)] translate-y-full sm:translate-y-0",
-  "w-10 bottom-0 h-[calc(100%-2.5rem)] -translate-x-full sm:translate-x-0",
+  "w-10 right-0 h-[calc(100%-2.5rem)] translate-x-full md:translate-x-0",
+  "h-10 right-0 bottom-0 w-[calc(100%-2.5rem)] translate-y-full md:translate-y-0",
+  "w-10 bottom-0 h-[calc(100%-2.5rem)] -translate-x-full md:translate-x-0",
 ];
 
 export default function OverlayBars() {
