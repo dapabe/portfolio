9f1b2f55d47b3ca51d848422fee017eca17f6ba3
DanBec <65568682+dapabe@users.noreply.github.com>
2022-07-21 19:24:06 -0300

    Fix i18n in ES translation

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,82 +4,108 @@ export default function About() {
   const t = useTranslations("/about");
   return (
     <section className="about noMaxWidth">
-      <section className="mx-auto mt-10 max-w-[75ch] space-y-6 px-4">
+      <div className="mx-auto mt-10 max-w-[75ch] space-y-6 px-4">
         <h2 className="paragraphTitle text-center underline underline-offset-4">
           {t("heading")}
         </h2>
-        <p>
-          {t("1st_paragraph")} <br />
-          <br />
-          {t("2nd_paragraph")} <br />
-          <br />
-          {t("3rd_paragraph.1st_text")}
-          <ExtLink
-            href={t("3rd_paragraph.1st_tag.href")}
-            title={t("3rd_paragraph.1st_tag.title")}
-          >
-            {t.rich("3rd_paragraph.1st_tag.inner_tag.rich", {
-              abbr: (text) => <abbr>{text}</abbr>,
-            })}
-          </ExtLink>
-          .
-        </p>
+        {t.rich("1st_paragraph", {
+          p: (text) => <p>{text}</p>,
+        })}
+        {t.rich("2nd_paragraph", {
+          p2: (text) => <p>{text}</p>,
+        })}
+        {t.rich("3rd_paragraph.rich", {
+          p3: (text) => <p>{text}</p>,
+          ExtLink: (text) => (
+            <ExtLink
+              href={t("3rd_paragraph.href")}
+              aria-label={t("3rd_paragraph.1st_a11y")}
+              title={t("3rd_paragraph.1st_a11y")}
+            >
+              {text}
+            </ExtLink>
+          ),
+          abbr: (text) => (
+            <abbr
+              aria-labelledby={t("3rd_paragraph.2nd_a11y")}
+              title={t("3rd_paragraph.2nd_a11y")}
+            >
+              {text}
+            </abbr>
+          ),
+        })}
         <h2 className="paragraphTitle text-center underline underline-offset-4">
           {t("2nd_heading")}
         </h2>
-        <p>
-          {t.rich("4th_paragraph.1st_tag.rich", {
-            AbbrLang: (text, rest) => (
-              <>
-                {rest}
-                <AbbrLang title={t("4th_paragraph.1st_tag.title")}>
-                  {text}
-                </AbbrLang>
-              </>
-            ),
-          })}
-          {t.rich("4th_paragraph.2nd_tag.rich", {
-            AbbrLang: (text) => (
-              <AbbrLang title={t("4th_paragraph.2nd_tag.title")}>
-                {text}
-              </AbbrLang>
-            ),
-          })}
-          <br />
-          {t.rich("5th_paragraph.1st_tag.rich", {
-            abbr: (text) => (
-              <abbr title={t("5th_paragraph.1st_tag.title")}>{text}</abbr>
-            ),
-          })}
-          {t.rich("5th_paragraph.2nd_tag.rich", {
-            abbr: (text) => (
-              <abbr title={t("5th_paragraph.2nd_tag.title")}>{text}</abbr>
-            ),
-          })}
-          {t.rich("5th_paragraph.3rd_tag.rich", {
-            AbbrLang: (text) => (
-              <AbbrLang title={t("5th_paragraph.3rd_tag.title")}>
-                {text}
-              </AbbrLang>
-            ),
-          })}
-          {t.rich("5th_paragraph.4th_tag.rich", {
-            ExtLink: (text) => (
-              <ExtLink href={t("5th_paragraph.4th_tag.href")}>{text}</ExtLink>
-            ),
-          })}
-          {t.rich("5th_paragraph.5th_tag.rich", {
-            ExtLink: (text) => (
-              <ExtLink href={t("5th_paragraph.5th_tag.href")}>{text}</ExtLink>
-            ),
-          })}
-          <br />
-          <br />
-          {t("5th_paragraph.1st_text")}
-          <br /> <br />
-          {t("5th_paragraph.2nd_text")}
-        </p>
-      </section>
+        {t.rich("4th_paragraph.rich", {
+          p4: (text) => <p>{text}</p>,
+          abbrlang1: (text) => (
+            <AbbrLang
+              aria-labelledby={t("4th_paragraph.1st_a11y")}
+              title={t("4th_paragraph.1st_a11y")}
+            >
+              {text}
+            </AbbrLang>
+          ),
+          abbrlang2: (text) => (
+            <AbbrLang
+              aria-labelledby={t("4th_paragraph.2nd_a11y")}
+              title={t("4th_paragraph.2nd_a11y")}
+            >
+              {text}
+            </AbbrLang>
+          ),
+        })}
+        {t.rich("5th_paragraph.rich", {
+          p5: (text) => <p>{text}</p>,
+          abbr: (text) => (
+            <abbr
+              aria-labelledby={t("5th_paragraph.1st_a11y")}
+              title={t("5th_paragraph.1st_a11y")}
+            >
+              {text}
+            </abbr>
+          ),
+          abbr2: (text) => (
+            <abbr
+              aria-labelledby={t("5th_paragraph.2nd_a11y")}
+              title={t("5th_paragraph.2nd_a11y")}
+            >
+              {text}
+            </abbr>
+          ),
+          abbrlang: (text) => (
+            <AbbrLang
+              aria-labelledby={t("5th_paragraph.3rd_a11y")}
+              title={t("5th_paragraph.3rd_a11y")}
+            >
+              {text}
+            </AbbrLang>
+          ),
+          extlink: (text) => (
+            <ExtLink
+              href={t("5th_paragraph.1st_link.href")}
+              aria-labelledby={t("5th_paragraph.1st_link.a11y")}
+              title={t("5th_paragraph.1st_link.a11y")}
+            >
+              {text}
+            </ExtLink>
+          ),
+          extlink2: (text) => (
+            <ExtLink
+              href={t("5th_paragraph.2nd_link.href")}
+              aria-labelledby={t("5th_paragraph.2nd_link.a11y")}
+              title={t("5th_paragraph.2nd_link.a11y")}
+            >
+              {text}
+            </ExtLink>
+          ),
+          br: () => <br></br>,
+        })}
+        {t.rich("notes", {
+          span: (text) => <span className="inline-block">{text}</span>,
+        })}
+      </div>
     </section>
   );
 }
